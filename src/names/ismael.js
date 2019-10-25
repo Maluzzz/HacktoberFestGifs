87151a147daf5d66c282030b671fd6a95503b7bb
ismabk <ismabk94@gmail.com>
2019-10-25 11:36:37 +0200

    cambio de ultima hora

--- src/names/ismael.js
+++ src/names/ismael.js
@@ -1,6 +1,6 @@
 export const isma = {
-  "name": "Ismael",
-  "skills": "react",
+  "name": "tra traa",
+  "skills": "Ismael",
   "gif": "https://media.tenor.com/images/1d16560997f10ec050ffdff745d205b7/tenor.gif"
 }
 
