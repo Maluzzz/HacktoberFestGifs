b4265eeb6a49d1889deffd904bb0d3a88a39abd1
Adrián Camacho Gil <33067500+adriancamacho3@users.noreply.github.com>
2019-10-25 12:11:15 +0200

    Update adrian.js

--- src/names/adrian.js
+++ src/names/adrian.js
@@ -1,4 +1,4 @@
- export const tu nombre = {
+ export const adrian = {
             "name": "Adrian",
             "skills": "Python",
             "gif": "https://media.giphy.com/media/l2Je0H39CPLlG4UkE/giphy.gif"
