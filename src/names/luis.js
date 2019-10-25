83fecc952229df11ca0ec77d28e60b45e39e61d1
kibadimundi <52224227+kibadimundi@users.noreply.github.com>
2019-10-25 11:31:21 +0200

    Create luis.js

---
+++ src/names/luis.js
@@ -0,0 +1,7 @@
+export const tu nombre = {
+            "name": "Luis",  
+            "skills": "VisualBasic", 
+            "gif": "http://giphygifs.s3.amazonaws.com/media/FjeGBljESVAzu/giphy.gif"
+
+        }
+        export default Luis
