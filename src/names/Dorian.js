7fcc09a321e7e91731770fe6fbf1d09978e12f2c
Doriaan94 <dorianfv94@hotmail.com>
2019-10-25 12:15:42 +0200

    Toma Gif

--- src/names/Dorian.js
+++ src/names/Dorian.js
@@ -1,7 +1,7 @@
- export const Dorian = {
+ export const dorian = {
             "name": "Dorian",  
             "skills": "C", 
             "gif": "http://gph.is/2md4erB"
 
         }
-        export default Dorian
+        export default dorian
