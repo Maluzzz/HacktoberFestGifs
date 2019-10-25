edc0e458bc1bea5f0310c05cfdc26cae74f08fe6
jvergara <josua1912@gmail.com>
2019-10-25 12:24:03 +0200

    correccion de fallos

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-export * from './vergara';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
