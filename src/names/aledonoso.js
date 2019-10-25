88196f856f2f355d74515a0dfc181787b7a37d5f
alesdonoso <aledonosan@gmail.com>
2019-10-25 11:17:55 +0200

    created ale donoso profile

---
+++ src/names/aledonoso.js
@@ -0,0 +1,7 @@
+export const aledonoso = {
+    "name": "Ale Donoso",
+    "skills": "CSS",
+    "gif": "http://giphygifs.s3.amazonaws.com/media/13FrpeVH09Zrb2/giphy.gif"
+}
+
+export default aledonoso
