8819df7d088a506ca600536642cd1fe30ccb4039
Ale Donoso <48650098+alesdonoso@users.noreply.github.com>
2020-10-30 18:27:27 +0100

    added paquito profile (#43)

---
+++ src/names/paquito.js
@@ -0,0 +1,7 @@
+export const paquito = {
+    "name": "paquito",
+    "skills": "CSS, HTML",
+    "gif": "http://giphygifs.s3.amazonaws.com/media/13FrpeVH09Zrb2/giphy.gif"
+}
+
+export default paquito
